600bbc744220d0a0e4bc74cf022990c76d0d13fa
obeybook <obeybook@gmail.com>
2020-04-22 01:05:34 +0900

    inputExample 다시 안보고 코딩

--- src/inputExample.js
+++ src/inputExample.js
@@ -1,55 +1,24 @@
 import React, { Component } from 'react';
 
-class InputExample extends Component {
+class inputExample extends Component{
     state = {
-        name : '',
-        take : false
-    }
-    shouldComponentUpdate(nextProps, nextState){
-        if(nextState.catch){
-            this.tkFnc();
-            return false; 
-        }else{
-            return true;
-        }
-    }
-    // componentWillUpdate(nextProps, nextState){
-        // console.log(nextState)
-    // }
-    // componentDidUpdate(prevProps, prevState){
-        // console.log(prevState)
-    // }
-
-    tkFnc = () => {
-        this.setState({
-            take : 'good'
-        })
+        name : ''
     }
 
-    changeFnc = (e) => {
+    inputChange = (e) => {
         this.setState({
             name : e.target.value
         })
-        if ( this.state.name === 'test') { 
-            this.setState({
-                catch : true
-            })
-        }else{
-            this.setState({
-                catch : false
-            })
-        }
     }
 
     render(){
-        return (
-            <form>
-                <input placeholder='name' value={this.state.name} onChange={this.changeFnc}/>
+        return(
+            <div>
+                <input type="text" value={this.state.name} onChange={this.inputChange}/>
                 <div>{this.state.name}</div>
-                <div>{this.state.take}</div>
-            </form>
+            </div>
         )
     }
 }
 
-export default InputExample;
+export default inputExample;
